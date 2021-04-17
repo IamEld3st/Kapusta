d5e2e061d3465ef0676ae94db512cd2c548d52ac
Samo Dobroň <34833780+enhaut@users.noreply.github.com>
2021-04-17 22:50:54 +0200

    Evaluation (#15)
    * added tests with parentheses
    
    * added screenshot of debugging
    
    * added evaluating of expression

--- src/kapusta_math/test_evaluation.py
+++ src/kapusta_math/test_evaluation.py
@@ -1,5 +1,18 @@
+import pytest
 from kapusta_math.evaluation import evaluate
 
 def test_basic_eval():
     assert evaluate("2+2") == 4
     assert evaluate("2*3") == 6
+    assert evaluate("1+2*3") == 7
+    assert evaluate("(1+2)*3") == 9
+    assert evaluate("4+2/2") == 5
+    assert evaluate("(4+2)/2") == 3
+
+    assert evaluate("(1*3*(2+2))/6+2") == 4
+    assert evaluate("(1*3*2+3)/6+2") == 3.5
+
+    with pytest.raises(SyntaxError):
+        evaluate("*3")
+    with pytest.raises(SyntaxError):
+        evaluate("(1*3")
