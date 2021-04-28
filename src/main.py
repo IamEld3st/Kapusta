ab1d9ceb6c3e6da5c44c2ad01187257e6b7ef9c4
IamEld3st <iameld3st@gmail.com>
2021-04-28 22:56:14 +0200

    Fixed stuff in main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
+import os
+import sys
+import platform
 import eel
 from kapusta_math.evaluation import evaluate
 
 if __name__ == '__main__':
-    eel.init("kapusta_gui")
-    eel.start("index.html", close_callback=lambda *args, **kwargs: exit(0), size=(100, 100))
+    gui_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'kapusta_gui')
+    eel.init(gui_folder)
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        pltf = platform.system()
+        if pltf == "Linux":
+            electron_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'chromium/chrome')
+        else:
+            electron_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'chromium/chrome.exe')
+        eel.browsers.set_path('chrome', electron_folder)
+    eel.start("index.html", mode="chrome", size=(325,425))
